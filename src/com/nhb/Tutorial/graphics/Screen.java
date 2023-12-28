23158ce03710b7bdde5cf4d3706da9ea9d165fba
Nhb <nhb_jr@yahoo.com>
2023-12-28 09:35:35 +0800

    Episode 9 - Rendering Pixels

--- src/com/nhb/Tutorial/graphics/Screen.java
+++ src/com/nhb/Tutorial/graphics/Screen.java
@@ -10,4 +10,12 @@ public class Screen {
         pixels = new int[width * height];
     }
 
+    public void render() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixels[x + y * width] = 0xFF00FF;
+            }
+        }
+    }
+
 }
